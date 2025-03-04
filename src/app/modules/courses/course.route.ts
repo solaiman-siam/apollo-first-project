fedfc4af062c4f2251971ccc9540b10d836e5552
solaiman-siam <solaimansiam93@gmail.com>
2025-03-04 06:08:39 +0600

    some changes

---
+++ src/app/modules/courses/course.route.ts
@@ -0,0 +1,17 @@
+
+import express from 'express'
+import { validateRequest } from '../../middleware/validateRequest'
+import { CourseValidation } from './course.validation'
+import { CourseController } from './course.controller'
+
+const router = express.Router()  
+
+
+router.post('/create-course', validateRequest(CourseValidation.createCourseValidationSchema), CourseController.createCourse)
+
+router.get('/:id', CourseController.getSingleCourse)
+router.delete('/:id', CourseController.deleteCourse)
+router.get('/', CourseController.getAllCourses)
+
+
+export const CourseRoutes = router
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -4,6 +4,7 @@ import { StudentRoute } from "../modules/student/student.route";
 import { AcademicSemisterRoute } from "../modules/academicSemister/academicSemister.route";
 import { AcademicFacultyRoutes } from "../modules/academicFaculty/academicFaculty.route";
 import { DepartmentRoutes } from "../modules/academicDepartment/academicDepartment.route";
+import { CourseRoutes } from "../modules/courses/course.route";
 
 
 const router = Router();
@@ -25,6 +26,10 @@ const moduleRoutes = [
     {
         path: '/academic-faculties',
         route: AcademicFacultyRoutes
+    },
+    {
+        path: '/courses',
+        route: CourseRoutes
     },
     {
         path: '/academic-departments',
